9cda1ec4ea67a12ca40a5cf384991304b2c32158
AbdulMoeedZafar <a.moeed01@gmail.com>
2017-10-10 17:06:30 -0400

    DC Motor Speed Control Under Implementation
    - flex_sensor file updated
    - Unvalidated changes made to dc_motor file

--- dc_motor.ino
+++ dc_motor.ino
@@ -1,10 +1,35 @@
 #include "sensorslab.h"
 
+int dc_len_of_period;                     // Microsecond
+int dc_len_of_pulse;                      // Microsecond
+int dc_len_of_pulse_start;                // Microsecond
+int dc_len_of_pulse_end;                  // Microsecond
+int dc_len_of_pulse_step;                 // Microsecond
+
+int t1;                                   // Duty-cycle Variables (Microseconds)
+int t2;                                   // Duty-cycle Variables (Microseconds)
+
+enum Speed
+{
+  DC_SPEED_LOW, DC_SPEED_MEDIUM, DC_SPEED_HIGH 
+};
+
 void dcMotorSetup()
 {
   pinMode(DC_MOTOR_EN, OUTPUT);
   pinMode(DC_MOTOR_1, OUTPUT);
   pinMode(DC_MOTOR_2, OUTPUT);
+  
+  dc_len_of_period = 25 * 1000;                      // Microsecond
+  dc_len_of_pulse = 0;                               // Microsecond
+  dc_len_of_pulse_start = 0.575 * 1000;              // Microsecond
+  dc_len_of_pulse_end = 2.350 * 1000;                // Microsecond
+  dc_len_of_pulse_step = 0.01 * 1000;                // Microsecond
+}
+
+void dcMotorSetSpeed()
+{
+  
 }
 
 void dcMotorControl()
--- flex_sensor.ino
+++ flex_sensor.ino
@@ -1,11 +1,15 @@
+#define FLEX_SENSOR_PIN A5
 
-void flexSensorSetup() {
-
+void flexSensorSetup() 
+{
+  pinMode(FLEX_SENSOR_PIN, INPUT);
 }
-
-
 
 void flexSensorControl() 
 {
-
+  int flex_sensor_val = analogRead(FLEX_SENSOR_PIN);
+  Serial.print("Flex sensor value: ");
+  Serial.println(flex_sensor_val);
 }
+
+
