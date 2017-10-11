af96c9e3768d9e0c450f8bb2c7a10e75d4da77da
Karthik_Paga <karthikpaga@gmail.com>
2017-10-10 23:48:16 -0400

    in-progress_ir_sharp_sensor

--- ir_sensor.ino
+++ ir_sensor.ino
@@ -1,8 +1,15 @@
+#include "SharpIR.h"
+
+#define ir A0
+#define model 20150
+
+SharpIR SharpIR(ir, model);
 
 void irSensorSetup() {
+    Serial.begin(9600);
 }
 
 
-void irSensorControl() 
+int irSensorControl() 
 {
 }
--- sketch_stepper_motor.ino
+++ sketch_stepper_motor.ino
@@ -10,25 +10,25 @@ void setup() {
 }
 
 void loop() {
-//  digitalWrite(enblPin, HIGH);
+/*  digitalWrite(enblPin, HIGH);*/// - do not touch the enable pin
   // Enables the motor to move in some direction
   digitalWrite(dirPin, HIGH);
   // Step 200 times to make one full rotation (MS1, MS2, MS3 - LOW)
-  for(int x = 0; x <200; x++){
+  for(int x = 0; x <3200; x++){
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
+    delayMicroseconds(100);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    delayMicroseconds(100);
   }
-  delay(2000); //one second delay
+  delay(1000); //one second delay
 
   digitalWrite(dirPin, LOW); // change te direction of rotation
   //make 2 full rotations 
-  for(int x = 0; x <400; x++){
+  for(int x = 0; x <6400; x++){
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
+    delayMicroseconds(100);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
+    delayMicroseconds(100);
   }
   delay(1000);
 
