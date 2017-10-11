8a0ca283bcdca94d7f5e529e531bde1a51799991
Karthik_Paga <karthikpaga@gmail.com>
2017-10-11 01:44:22 -0400

    ir_sensor and stepper_motor

--- ir_sensor.ino
+++ ir_sensor.ino
@@ -12,4 +12,18 @@ void irSensorSetup() {
 
 int irSensorControl() 
 {
+    delay(2000);   
+
+  unsigned long pepe1=millis();  // takes the time before the loop on the library begins
+
+  int dis=SharpIR.distance();  // this returns the distance to the object you're measuring
+
+
+//  Serial.print("Mean distance: ");  // returns it to the serial monitor
+//  Serial.println(dis);
+return dis;
+  
+//  unsigned long pepe2=millis()-pepe1;  // the following gives you the time taken to get the measurement
+//  Serial.print("Time taken (ms): ");
+//  Serial.println(pepe2);
 }
