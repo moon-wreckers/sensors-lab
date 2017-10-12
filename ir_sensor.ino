e7455330bdfe81c42f4ad7f61e94c65535521372
Karthik_Paga <karthikpaga@gmail.com>
2017-10-12 04:15:55 -0400

    calibrated IR range (10 to 80 cm) - verified till 55cm

--- ir_sensor.ino
+++ ir_sensor.ino
@@ -1,25 +1,40 @@
-#include "SharpIR.h"
-
-#define model 20150
-
-SharpIR SharpIR(IR_PIN, model);
+int sensorPin = A0;
 
 void irSensorSetup() {
+    Serial.begin(9600);
 }
 
 
 int irSensorControl() 
-{
-  unsigned long pepe1=millis();  // takes the time before the loop on the library begins
-
-  int dis=SharpIR.distance();  // this returns the distance to the object you're measuring
-
-
-//  Serial.print("Mean distance: ");  // returns it to the serial monitor
-//  Serial.println(dis);
-return dis;
+{   
+  int count = 2000;
+  float dis = 0;
+  int mV = 0;
+  float distance = 0;
   
-//  unsigned long pepe2=millis()-pepe1;  // the following gives you the time taken to get the measurement
-//  Serial.print("Time taken (ms): ");
-//  Serial.println(pepe2);
+  //-----coefficients
+  float p1, p2, p3;
+
+// ---------Applying a low pass filter
+  while (count>0){
+    dis= dis + (map(analogRead(sensorPin), 0, 1023, 0, 5000));  // this returns the distance to the object you're measuring
+    count--;
+  }
+  dis = (dis/2000);
+  mV = round(dis);
+
+  // Experimentally optained
+  p1 = 0.000008487;
+  p2 = -0.05665;
+  p3 = 106.5;
+
+//-----calculate distance
+  distance = ((p1*mV*mV) + (p2*mV) + p3);
+
+//  Serial.print("Mean 200 readings (milli Volts): ");  // returns it to the serial monitor
+//  Serial.println(mV);
+//
+//  Serial.print("Distance Prediction: ");  // returns it to the serial monitor
+//  Serial.println(round(distance));
+return round(distance);
 }
