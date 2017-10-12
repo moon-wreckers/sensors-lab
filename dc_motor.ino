342284f71786cb1d834c1c7a5753a03f214789fb
AbdulMoeedZafar <a.moeed01@gmail.com>
2017-10-11 22:44:37 -0400

    dcSpeedControl
    For Matt

--- dc_motor.ino
+++ dc_motor.ino
@@ -13,6 +13,11 @@ const double k_p = 0.8;
 const double k_i = 0.1;
 const double k_d = 0.1;
 
+long current_time = 0;
+long current_encoder_pos;
+long last_time = 0;
+long last_encoder_pos;
+
 
 
 void dcMotorSetup()
@@ -132,4 +137,29 @@ void dcMotorTestFunc()
     }
   
   printEncoderPosition();
+}
+
+void dcSpeedControl(int desired_vel)
+{
+  current_time = millis();
+  current_encoder_pos = _encoder_pos;
+  
+  timestep = current_time - last_time;
+  distance = current_encoder_pos - last_encoder_pos;
+  
+  double current_vel = (distance * 1000) / timestep;  // degrees/second
+  
+  last_time = millis();
+  last_encoder_pos = _encoder_pos;
+  
+  int voltage = 140;
+  
+  if(current_vel - desired_vel > 3)
+    voltage--;
+  else if(current_vel - desired_vel < 3)
+    voltage++;
+  
+  rot_dir direction_ = (desired_vel<abs(desired_vel))? CLOCKWISE : COUNTERCLOCKWISE;
+  dcMotorDrive((int)voltage, direction_);
+    
 }
